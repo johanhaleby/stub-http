c8723014a3766b6cfdd3b272281b303b297319e4
Johan Haleby <johan.haleby@gmail.com>
2016-03-13 15:56:25 +0100

    Pointing to mit license

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject se.haleby/fake-http "0.1.0-SNAPSHOT"
   :description "A client library agonistic way to fake HTTP responses"
   :url "https://github.com/johanhaleby/fake-http"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :lein-release {:deploy-via :shell
-                 :shell      ["lein" "deploy" "clojars"]}
+  :license {:name         "MIT License"
+            :url          "http://www.opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :lein-release {:deploy-via :clojars}
   :min-lein-version "2.0.0"
   :dependencies [[org.nanohttpd/nanohttpd "2.3.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
