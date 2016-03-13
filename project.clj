6487786cc9957c4097fc6a947170b6f5b4d1a248
Johan Haleby <johan.haleby@gmail.com>
2016-03-13 11:28:09 +0100

    Added lein release settings

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/johanhaleby/fake-http"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :lein-release {:deploy-via :clojars}
   :min-lein-version "2.0.0"
   :dependencies [[org.nanohttpd/nanohttpd "2.3.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
