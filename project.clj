676dfd865635e98777a2fc6b74861bbb89f21470
Johan Haleby <johan.haleby@gmail.com>
2016-03-21 10:33:13 +0100

    lein-release plugin: bumped version from 0.1.4 to 0.1.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject se.haleby/fake-http "0.1.4"
+(defproject se.haleby/fake-http "0.1.5-SNAPSHOT"
   :description "A client library agonistic way to fake HTTP responses"
   :url "https://github.com/johanhaleby/fake-http"
   :license {:name         "MIT License"
