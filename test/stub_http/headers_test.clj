2eb04fcadd9c4b6c850f489df2106c77b2abeb0c
Johan Haleby <johan.haleby@gmail.com>
2017-12-01 12:39:42 +0100

    Improved tests

--- test/stub_http/headers_test.clj
+++ test/stub_http/headers_test.clj
@@ -3,10 +3,24 @@
             [stub-http.core :refer :all]
             [clj-http.lite.client :as client]))
 
-(deftest MethodMatching
-  (testing "routes can match on method"
+(deftest HeaderMatching
+  (testing "headers can be defined as string - string pairs"
     (with-routes!
       {"/something" {:status 200 :content-type "text/plain" :body "hello" :headers {"john" "doe" "ikk" "ikk2"}}}
       (let [headers (:headers (client/get (str uri "/something")))]
         (is (= "doe" (get headers "john")))
-        (is (= "ikk2" (get headers "ikk")))))))
+        (is (= "ikk2" (get headers "ikk"))))))
+
+  (testing "headers can be defined as keyword - string pairs"
+    (with-routes!
+      {"/something" {:status 200 :content-type "text/plain" :body "hello" :headers {:john "doe" :ikk "ikk2"}}}
+      (let [headers (:headers (client/get (str uri "/something")))]
+        (is (= "doe" (get headers "john")))
+        (is (= "ikk2" (get headers "ikk"))))))
+
+  (testing "headers can be defined as keyword - int pairs"
+    (with-routes!
+      {"/something" {:status 200 :content-type "text/plain" :body "hello" :headers {"john" 2 :ikk 3}}}
+      (let [headers (:headers (client/get (str uri "/something")))]
+        (is (= "2" (get headers "john")))
+        (is (= "3" (get headers "ikk")))))))
