fa3c8a50be9c39b15fef078678ea58d8128358ff
Ricardo Mayerhofer <ricardo.mayerhofer@ideais.com.br>
2018-03-22 06:10:43 -0300

    Counter implementation attempt.

---
+++ test/stub_http/call_counter_test.clj
@@ -0,0 +1,15 @@
+(ns stub-http.call-counter-test
+  (:require [clojure.test :refer :all]
+            [stub-http.core :refer :all]
+            [cheshire.core :as json]
+            [clj-http.lite.client :as client]))
+
+
+(deftest CounterTest
+  (testing "records call count"
+    (let [counter (atom 0)]
+      (with-routes!
+        [body (json/generate-string {:hello "world"})]
+        {"/something" {:status 200 :content-type "application/json"
+                       :body   body :counter counter}}
+        (is (= @counter 1 ))))))
