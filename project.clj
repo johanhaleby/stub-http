d0ac2719daaf4e38aa53ed92f6463ec6fd0599f0
Johan Haleby <johan.haleby@gmail.com>
2020-08-19 21:06:05 +0200

    Updating to 0.2.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject se.haleby/stub-http "0.2.9"
+(defproject se.haleby/stub-http "0.2.10-SNAPSHOT"
   :description "A client library agonistic way to stub HTTP responses"
   :url "https://github.com/johanhaleby/stub-http"
   :license {:name         "MIT License"
