4f184105dac6f5a98eb6ba8bc4b4f081e5ff8844
jonassvalin <jonassvalin@gmail.com>
2020-08-19 19:02:34 +0200

    Add support for matching request body (#13)

---
+++ test/stub_http/body_test.clj
@@ -0,0 +1,29 @@
+(ns stub-http.body-test
+  (:require [clojure.test :refer :all]
+            [stub-http.core :refer :all]
+            [cheshire.core :as json]
+            [clj-http.lite.client :as client]))
+
+(deftest BodyMatching
+  (testing "routes can match on body"
+    (with-routes!
+      {{:path   "/something"
+        :body   (json/generate-string {:some-property "some-value"})}
+       {:status 201 :content-type "application/json" :body (json/generate-string {:hello "world1"})}
+       {:path   "/something"
+        :body   (json/generate-string {:some-property "different-value"})}
+       {:status 201 :content-type "application/json" :body (json/generate-string {:hello "world2"})}}
+      (let [response1 (->
+                        (str uri "/something")
+                        (client/post {:body         (json/generate-string {:some-property "some-value"})
+                                      :content-type :json})
+                        :body
+                        (json/parse-string true))
+            response2 (->
+                        (str uri "/something")
+                        (client/post {:body         (json/generate-string {:some-property "different-value"})
+                                      :content-type :json})
+                        :body
+                        (json/parse-string true))]
+        (is (= "world1" (:hello response1)))
+        (is (= "world2" (:hello response2)))))))
